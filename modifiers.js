6dcd1ebf075412ff30afe6541231e33558c30b12
Peter Orum <peter@peterorum.com>
2015-02-22 17:56:07 +1100

    get real

--- modifiers.js
+++ modifiers.js
@@ -33,89 +33,95 @@
     }, ];
 
     exports.modifiers = [
-    {
-        fn: function angle(functal, result)
         {
-            var vals = fp.map(function(z)
+            fn: function angle(functal, result)
             {
-                return math.atan2(z.re, z.im) / math.pi;
-            }, result.zs);
-
-            return functal.modifierReduce(vals);
-        },
-        weight: 1,
-    },
-    {
-        fn: function real(functal, result)
-        {
-            var vals = fp.map(function(z)
-            {
-                return z.re;
-            }, result.zs);
-
-            return functal.modifierReduce(vals);
-        },
-        weight: 1000000,
-    },
-
-    {
-        fn: function norm(functal, result)
-        {
-            var vals = fp.map(function(z)
-            {
-                return math.norm(z) / functal.limit;
-            }, result.zs);
-
-            return functal.modifierReduce(vals);
-        },
-        weight: 1,
-    },
-    {
-        // circle trap
-
-        fn: (function()
-        {
-            var params = {};
-
-            var fn = function circleTrap(functal, result)
-            {
-                var x;
-
                 var vals = fp.map(function(z)
                 {
-                    var z1 = math.subtract(z, params.centre);
-
-                    var distance = math.sqrt(math.norm(z1));
-
-                    if (math.abs(distance - params.diameter) < params.band)
-                    {
-                        x = math.max(-1, math.min(1, distance - params.diameter));
-                    }
-                    else
-                    {
-                        x = 0;
-                    }
-
-                    return x;
+                    return math.atan2(z.re, z.im) / math.pi;
                 }, result.zs);
 
                 return functal.modifierReduce(vals);
-            };
-
-            fn.params = params;
-
-            fn.setParams = function()
+            },
+            weight: 1,
+        },
+        {
+            fn: function real(functal, result)
             {
-                params.diameter = fp.bandom(1, -2);
-                params.band = fp.bandom(1, -3);
-                params.centre = math.complex(fp.bandom(1, 2) * fp.randomSign() - 1, fp.bandom(1, 2) * fp.randomSign());
-            };
+                var max = math.max(fp.map(function(z)
+                {
+                    return math.abs(z.re);
+                }, result.zs));
 
-            fn.setParams();
+                var vals = fp.map(function(z)
+                {
+                    return z.re / max;
+                }, result.zs);
 
-            return fn;
-        })(),
-        weight: 1,
-    }];
+                return functal.modifierReduce(vals);
+            },
+            weight: 1,
+        },
+
+        {
+            fn: function norm(functal, result)
+            {
+                var vals = fp.map(function(z)
+                {
+                    return math.norm(z) / functal.limit;
+                }, result.zs);
+
+                return functal.modifierReduce(vals);
+            },
+            weight: 1,
+        },
+        {
+            // circle trap
+
+            fn: (function()
+            {
+                var params = {};
+
+                var fn = function circleTrap(functal, result)
+                {
+                    var x;
+
+                    var vals = fp.map(function(z)
+                    {
+                        var z1 = math.subtract(z, params.centre);
+
+                        var distance = math.sqrt(math.norm(z1));
+
+                        if (math.abs(distance - params.diameter) < params.band)
+                        {
+                            x = math.max(-1, math.min(1, distance - params.diameter));
+                        }
+                        else
+                        {
+                            x = 0;
+                        }
+
+                        return x;
+                    }, result.zs);
+
+                    return functal.modifierReduce(vals);
+                };
+
+                fn.params = params;
+
+                fn.setParams = function()
+                {
+                    params.diameter = fp.bandom(1, -2);
+                    params.band = fp.bandom(1, -3);
+                    params.centre = math.complex(fp.bandom(1, 2) * fp.randomSign() - 1, fp.bandom(1, 2) * fp.randomSign());
+                };
+
+                fn.setParams();
+
+                return fn;
+            })(),
+            weight: 1,
+        }
+    ];
 
 }());
