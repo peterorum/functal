a000dd6d16178eff214dec2bc287a09b60c01ec5
Peter Orum <peter@peterorum.com>
2015-02-28 11:51:35 +1100

    limit to 100

--- fsq.js
+++ fsq.js
@@ -10,6 +10,7 @@
     exports.readFile = Q.nfbind(fs.readFile);
     exports.writeFile = Q.nfbind(fs.writeFile);
     exports.readdir = Q.nfbind(fs.readdir);
+    exports.readdirSync = Q.nfbind(fs.readdirSync);
     exports.unlink = Q.nfbind(fs.unlink);
 
 })();
