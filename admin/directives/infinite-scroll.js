93931ab226586ad8e5ee6c1110ab3fec4a674692
Peter Orum <peter@peterorum.com>
2015-05-16 16:14:14 +1000

    document directive

--- admin/directives/infinite-scroll.js
+++ admin/directives/infinite-scroll.js
@@ -3,11 +3,14 @@
     {
         "use strict";
 
+        // usage: add this at bottom of page
+        // <div infinite-scroll="yourIncrementer()"></div>
+
         angular.module('functalApp')
             .directive('infiniteScroll', ["$window", "$document", function($window, $document)
             {
                 return {
-                    restrict: 'EA',
+                    restrict: 'A',
                     scope:
                     {
                         infiniteScroll: "&"
@@ -17,6 +20,8 @@
                     {
                         $($window).on('scroll', function()
                         {
+                            // fire event when top of div reaches bottom of visible page
+
                             if ($(element).position().top - $($window).scrollTop() - $($document)[0].body.clientHeight <= 0)
                             {
                                 scope.infiniteScroll();
