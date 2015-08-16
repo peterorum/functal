cdcbb2101e5139a83d1a98b385cbf509448057f3
Peter Orum <peter@peterorum.com>
2015-08-16 15:07:24 +1000

    get & store tweets

--- titles/get-tweets.py
+++ titles/get-tweets.py
@@ -5,7 +5,7 @@ import random
 import time
 # import re
 # import sys
-#import json
+# import json
 import pprint
 
 import twitter
@@ -26,13 +26,25 @@ def get_tweets(topic):
     # print('search_results')
     # pp.pprint(search_results)
 
-    texts = [{'id': tweet['id_str'], 'text': tweet['text'], 'user': tweet['user']['name']}
+    # 'user': tweet['user']['name']
+    texts = [{'_id': tweet['id_str'], 'text': tweet['text'], 'topic': topic}
              for tweet in search_results['statuses'] if topic in tweet['text']]
 
     print('tweets: ' + str(len(texts)))
 
     if len(texts) > 0:
         pp.pprint(texts)
+
+        # store
+        db = client['topics']
+        tweets = db['tweets']
+
+        # ignore dup key error
+        try:
+            result = tweets.insert(texts, {'ordered': False})
+            print(str(len(result)) + ' tweets inserted')
+        except:
+            print('db insert error ignored')
 
 #--- global
 
@@ -46,15 +58,13 @@ twit = twitter.Twitter(auth=auth)
 
 def main():
 
-    db = client('topics')
+    topics = ["red", "orange", "yellow", "green", "blue", "purple",
+              "pink", "triangle", "square", "circle", "arrow", "asterisk", "wavy", "star"]
 
-    # topics = ["red", "orange", "yellow", "green", "blue", "purple",
-    #          "pink", "triangle", "square", "circle", "arrow", "asterisk", "wavy", "star"]
-
-    # while True:
-    # get_tweets('triangle')
-    #    get_tweets(topics[random.randint(0, len(topics) - 1)])
-    #    time.sleep(60)
+    while True:
+        # get_tweets('red')
+        get_tweets(topics[random.randint(0, len(topics) - 1)])
+        time.sleep(60)
 
 #--- run
 
