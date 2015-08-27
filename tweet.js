8bdb10666a36864170b5d98f375672b5029062ff
Peter Orum <peter@peterorum.com>
2015-08-27 22:10:48 +1000

    title tweet

--- tweet.js
+++ tweet.js
@@ -4,42 +4,54 @@
   // tweet & delete oldest file
   // download it from s3 & delete it
 
-  var fsq = require('./fsq');
+  // var fsq = require('./fsq');
   var s3 = require('./s3client');
   var twit = require('./tweet-media');
+
+  var promise = require("bluebird");
+  var mongodb = promise.promisifyAll(require("mongodb"));
+  var mongoClient = promise.promisifyAll(mongodb.MongoClient);
 
   var msg = '#fractal #functal #digitalart iPhone app https://bit.ly/dailyfunctal';
 
   var bucket = 'functal-images';
-  var bucketJson = 'functal-json';
+  // var bucketJson = 'functal-json';
 
-  s3.list('functal-images').then(function(result) {
-    if (result.count === 0) {
-      console.log('No files');
-    }
-    else {
-      var r = Math.floor(Math.random() * result.files.length);
-      var key = result.files[r].Key;
+  mongoClient.connectAsync(process.env.mongo_functal).then(function(client) {
 
-      var tmpFile = '/tmp/tweet-' + key;
+    var db = client.db('functal');
 
-      s3.download(bucket, key, tmpFile).then(function() {
-        twit.tweet(msg, tmpFile, function() {
-          // delete image
-          // s3.delete(bucket, key)
-          //     .then(function()
-          //     {
-          //         // delete json
-          //         return s3.delete(bucketJson, key.replace(/(png|jpg)$/, 'json'));
-          //     })
-          //     .then(function()
-          //     {
-          //         // delete local file
-          //         return fsq.unlink(tmpFile);
-          //     })
-          //     .done();
+    s3.list('functal-images').then(function(result) {
+      if (result.count === 0) {
+        console.log('No files');
+      }
+      else {
+        var r = Math.floor(Math.random() * result.files.length);
+        var key = result.files[r].Key;
+
+        var tmpFile = '/tmp/tweet-' + key;
+
+        console.log(key);
+
+        s3.download(bucket, key, tmpFile).then(function() {
+
+          // prefix msg with title if any
+          db.collection('images').findOneAsync(
+            {
+              name: key
+            }).then(function(image) {
+            if (image && image.title) {
+              msg = '"' + image.title + '"" ' + msg;
+            }
+
+            console.log(msg);
+
+            // twit.tweet(msg, tmpFile, function() {});
+
+            client.close();
+          });
         });
-      });
-    }
+      }
+    });
   });
 }());
