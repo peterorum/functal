51be18aff390a7a86e6b6c5559e29dd3cbad5be5
Peter Orum <peter@peterorum.com>
2015-12-31 11:13:37 +1100

    fix syntax

--- titles/fill-titles.py
+++ titles/fill-titles.py
@@ -38,8 +38,7 @@ def get_functals():
 
 
 def get_functals_without_title():
-    # return list(db_functal.images.find({'title': {'$exists': False}}))
-    return list(db_functal.images.find('$or': [{'title': {'$exists': False}}, {'title': {'$eq': ''}}]}))
+    return list(db_functal.images.find({'$or': [{'title': {'$exists': False}}, {'title': {'$eq': ''}}]}))
 
 #--- run
 
