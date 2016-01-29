18d65bc608d01ff7a3f2cd25860c25ee8083ae51
Peter Orum <peter@peterorum.com>
2016-01-29 01:32:36 +0000

    add shutdown

--- w-gpu-create-3d.ps1
+++ w-gpu-create-3d.ps1
@@ -9,28 +9,35 @@ Write-Host "Loading files....";
 $files = @(get-childitem -recurse -path $path -filter $filter)
 
 foreach ($file in $files) {
-    $count ++
-    $f = [io.path]::GetFileNameWithoutExtension($file)
-    Write-Host $f;
+    $count ++;
+    $f = [io.path]::GetFileNameWithoutExtension($file);
+
+    Write-Host "Making 3d from "$f;
 
     node 3d-image.js $path$f".jpg";
 
     # saves as 768x1029
+    write-host "Saving png"
+
     node screenshot $path$f;
 
     # crop bottom off to 768x1024 & convert to jpg
+    write-host "Cropping to jpg"
+
     convert $path$f".png" -gravity south -chop 0x5 $path$f"-3d.jpg"
 
-#    write-host "Moving to s3"
-#
-#    aws s3 cp $path$f"-3d.jpg" s3://functal-images --acl="public-read"
-#
-#    write-host "Deleting"
-#
-#    remove-item $path$f".jpg"
-#    remove-item $path$f".html"
-#    remove-item $path$f".png"
-#    remove-item $path$f"-3d.jpg"
+    write-host "Moving to s3"
+
+    aws s3 cp $path$f"-3d.jpg" s3://functal-images --acl="public-read"
+
+    write-host "Deleting"
+
+    remove-item $path$f".jpg"
+    remove-item $path$f".html"
+    remove-item $path$f".png"
+    remove-item $path$f"-3d.jpg"
 }
 
 Write-Host "$count files were processed"
+
+shutdown /s
